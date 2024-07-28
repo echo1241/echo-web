cbe8b4bebffda12daa1f0b04c70a4394e641ea9e
hyun <hyun1202a@gmail.com>
2024-07-28 18:15:20 +0900

    intro Link 적용

--- src/page/intro/Intro.jsx
+++ src/page/intro/Intro.jsx
@@ -1,6 +1,8 @@
 import "./intro.css";
+import { Link } from 'react-router-dom';
 
 function Intro() {
+
     return (
         <div className="card">
             <div className="circle">
@@ -9,7 +11,7 @@ function Intro() {
             <div className="content">
                 <h2>Echo</h2>
                 <p>모든 소리를 하나로 담아내어 소통의 순간을 하나로 이어주는 텍스트부터 화상까지 완벽한 통합 커뮤니케이션 플랫폼</p>
-                <a href="../LoginPage/Login.html">Let's go</a>
+                <Link to="/login">Let's go</Link>
             </div>
             <img src="images/mic-bg.png" alt="" className="product_img"></img>
         </div>
