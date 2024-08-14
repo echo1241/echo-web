5ef1f2442575450c92001c6cf2ecd69503f45354
Berithx <seere7777@gmail.com>
2024-08-14 14:42:24 +0900

    fix: 채팅 도메인 css 및 스크립트 수정 - 타이핑 추적 렌더링 요소 배치 - 이미지 업로드 프리뷰  css 수정 - 텍스트 이미지 하이퍼링크 기능 추가

--- src/component/textchat/TextChat.jsx
+++ src/component/textchat/TextChat.jsx
@@ -11,7 +11,7 @@ export const EnterTextChannel = ({ channelId, channelName }) => {
     const [previewImgUrl, setPreviewImgUrl] = useState(null);
     const [error, setError] = useState('');
     const [isChatTextDisabled, setIsChatTextDisabled] = useState(false);
-    const messagesEndRef = useRef(null);
+    const [typingUsers, setTypingUsers] = useState(new Set()); // 여러 사용자의 타이핑 상태를 관리
     const fileRef = useRef(null);
     const ws = useRef(null);
 
@@ -19,10 +19,9 @@ export const EnterTextChannel = ({ channelId, channelName }) => {
 
     useEffect(() => {
         if (channelId) {
-            // 메시지 리스트 초기화
-            if (messages) {
-                setMessages([]);
-            }
+            setMessages([]);
+            setTypingUsers(new Set());
+            setInputValue('');
             connectWebSocket(channelId);
         }
 
@@ -49,14 +48,25 @@ export const EnterTextChannel = ({ channelId, channelName }) => {
             socket.onmessage = (event) => {
 
                 const data = JSON.parse(event.data);
+                if (data.typing) {
+                    setTypingUsers(prev => new Set(prev).add(data.username)); // 타이핑 중인 사용자 추가
+                } else if (data.typing === false) {
+                    setTypingUsers(prev => {
+                        const updated = new Set(prev);
+                        updated.delete(data.username); // 타이핑 중지된 사용자 제거
+                        return updated;
+                    });
+                }
 
-                setMessages(prevMessages => [...prevMessages, {
-                    id: data.id,
-                    username: data.username,
-                    contents: data.contents,
-                    timestamp: formatDate(new Date(data.createdAt)),
-                    type: data.type
-                }]);
+                if (data.contents) {
+                    setMessages(prevMessages => [...prevMessages, {
+                        id: data.id,
+                        username: data.username,
+                        contents: data.contents,
+                        timestamp: formatDate(new Date(data.createdAt)),
+                        type: data.type
+                    }]);
+                }
             };
 
             socket.onclose = () => {
@@ -76,6 +86,21 @@ export const EnterTextChannel = ({ channelId, channelName }) => {
 
     const handleInputChange = (event) => {
         setInputValue(event.target.value);
+
+        if (event.target.value.trim() === '') {
+            sendTypingStatus(false); // 입력이 비어있다면 타이핑 중지 메시지 전송
+        } else if (!typingUsers.has(token)) {
+            sendTypingStatus(true); // 입력이 시작되면 타이핑 중 메시지 전송
+        }
+    };
+
+    const sendTypingStatus = (isTyping) => {
+        if (ws.current) {
+            const data = {
+                typing: isTyping,
+            };
+            ws.current.send(JSON.stringify(data));
+        }
     };
 
     const handleSendMessage = async (event) => {
@@ -85,6 +110,7 @@ export const EnterTextChannel = ({ channelId, channelName }) => {
             }
             ws.current.send(JSON.stringify(data));
             setInputValue('');
+            sendTypingStatus(false);
         } else if (postImg && inputValue === '') {
             event.preventDefault();
 
@@ -98,8 +124,11 @@ export const EnterTextChannel = ({ channelId, channelName }) => {
     };
 
     const handleKeyPress = (event) => {
+        if (event.isComposing || event.keyCode === 229) {
+            return;
+        }
+
         if (event.key === 'Enter') {
-            console.log('엔터 눌림')
             handleSendMessage(event);
         }
     };
@@ -166,15 +195,14 @@ export const EnterTextChannel = ({ channelId, channelName }) => {
                         {msg.type === 'TEXT' ? (
                             <p>{msg.contents}</p>
                         ) : (
-                            <p>
+                            <a href={msg.contents} target="_blank" rel="noopener noreferrer">
                                 <img src={msg.contents}></img>
-                            </p>
+                            </a>
                         )}
                         <hr className="message-line" />
                     </div>
                 ))}
             </div>
-
             <div className="msg-wrap">
                 {error && (
                     <div className="upload-error">
@@ -194,7 +222,7 @@ export const EnterTextChannel = ({ channelId, channelName }) => {
                     </div>
                     <input type="text"
                         className="chat-text"
-                        placeholder={`${channelName} 에 메시지 보내기`}
+                        placeholder={`${channelName}에 메시지 보내기`}
                         value={inputValue}
                         onChange={handleInputChange}
                         onKeyDown={handleKeyPress}
@@ -202,6 +230,13 @@ export const EnterTextChannel = ({ channelId, channelName }) => {
                     />
                     <button className="send icon cell" onClick={handleSendMessage}></button>
                 </div>
+            </div>
+            <div className="typing-container">
+                {typingUsers.size > 0 && (
+                    <div className="typing-indicator">
+                        <p>{[...typingUsers].join(', ')}님이 입력 중입니다...</p>
+                    </div>
+                )}
             </div>
         </>
     )
