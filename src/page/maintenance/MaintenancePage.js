c030bc00c074c7cb9c5f496ab5c756739bcfde96
hyun <apfhd0257@naver.com>
2024-08-18 12:19:35 +0900

    fix: 점검 페이지 css 수정

--- src/page/maintenance/MaintenancePage.js
+++ src/page/maintenance/MaintenancePage.js
@@ -1,15 +1,16 @@
-import React from 'react';
 import './MaintenancePage.css';
 
 function MaintenancePage() {
     return (
-        <div className="vh">
-            <div>
-                <div className="wrap">
-                    <h1>ECHO 서비스 점검중 입니다</h1>
-                    <h2><p>보다 안정적인 서비스 제공을 위해 서비스 점검을 진행 중 입니다<br/>이용에 불편을 드려 대단히 죄송합니다<br/><br/></p></h2>
-                    <p>서비스 점검시간
-                        00:00~09:00</p>
+        <div className="maintenance-wrap">
+            <div className="vh">
+                <div>
+                    <div className="wrap">
+                        <h1>ECHO 서비스 점검중 입니다</h1>
+                        <h2><p>보다 안정적인 서비스 제공을 위해 서비스 점검을 진행 중 입니다<br/>이용에 불편을 드려 대단히 죄송합니다<br/><br/></p></h2>
+                        <p>서비스 점검시간
+                            00:00~09:00</p>
+                    </div>
                 </div>
             </div>
         </div>
