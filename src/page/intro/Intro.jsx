ecd3e55629804d59f2f48439c5fc11402fb0f7b0
HyeonjinChoi <eaglushyeonjin@gmail.com>
2024-08-21 15:46:16 +0900

    fix: 인트로 수정

--- src/page/intro/Intro.jsx
+++ src/page/intro/Intro.jsx
@@ -18,7 +18,7 @@ function Intro() {
             </div>
             <div className="content">
                 <h2>Echo</h2>
-                <p>모든 소리를 하나로 담아내어 소통의 순간을 하나로 이어주는 텍스트부터 화상까지 완벽한 통합 커뮤니케이션 플랫폼</p>
+                <p>모든 소리를 하나로 담아내어<br/>소통의 순간을 하나로 이어주는<br/>통합 커뮤니케이션 플랫폼</p>
                 <Link to="/login">Let's go</Link>
             </div>
             <img src="images/mic-bg.png" alt="" className="product_img"></img>
